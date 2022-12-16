c338db91fd45d085881e572fe2343937105d4d87
St <St@ya.ru>
2022-12-16 22:47:01 +0300

    вторая задача

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,11 +1,32 @@
-﻿Console.WriteLine("Введите первое число");
-int m=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int n=Convert.ToInt32(Console.ReadLine());
+﻿// ПЕРВАЯ ЗАДАЧА
 
-if (m>n) {
-    Console.WriteLine("Первое число больше второго");
+// Console.WriteLine("Введите первое число");
+// int m=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int n=Convert.ToInt32(Console.ReadLine());
+
+// if (m>n) {
+//     Console.WriteLine("Первое число больше второго");
+// }
+// else {
+//     Console.WriteLine("Второе число больше первого");
+// }
+
+
+// ВТОРАЯ ЗАДАЧА
+
+ Console.WriteLine("Введите первое число");
+ int a=Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите второе число");
+ int b=Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Введите третье число");
+ int c=Convert.ToInt32(Console.ReadLine());
+int max =a;
+if (b>max){
+    max=b;
 }
-else {
-    Console.WriteLine("Второе число больше первого");
+if (c>max)
+{
+    max=c;
 }
+Console.WriteLine($"Максимальное число= {max}"); 
