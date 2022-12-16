c57df150bc2381710b15127d5ac9c92ed9359e06
St <St@ya.ru>
2022-12-17 00:31:46 +0300

    третья и четвертая задача

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -15,18 +15,49 @@
 
 // ВТОРАЯ ЗАДАЧА
 
- Console.WriteLine("Введите первое число");
- int a=Convert.ToInt32(Console.ReadLine());
- Console.WriteLine("Введите второе число");
- int b=Convert.ToInt32(Console.ReadLine());
-  Console.WriteLine("Введите третье число");
- int c=Convert.ToInt32(Console.ReadLine());
-int max =a;
-if (b>max){
-    max=b;
-}
-if (c>max)
+//  Console.WriteLine("Введите первое число");
+//  int a=Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите второе число");
+//  int b=Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine("Введите третье число");
+//  int c=Convert.ToInt32(Console.ReadLine());
+// int max =a;
+// if (b>max){
+//     max=b;
+// }
+// if (c>max)
+// {
+//     max=c;
+// }
+// Console.WriteLine($"Максимальное число= {max}"); 
+
+// ТРЕТЬЯ ЗАДАЧА
+
+// Console.WriteLine("Введите число");
+// int a=Convert.ToInt32(Console.ReadLine());
+// if (a%2==0)
+// {
+//     Console.WriteLine($"Число {a} четное");
+// }
+// else
+// {
+// Console.WriteLine($"Число {a} нечетное");
+// }
+
+//ЧЕТВЕРТАЯ ЗАДАЧА
+
+Console.Write("Введите число ");
+int a =Convert.ToInt32(Console.ReadLine());
+int i=1;
+Console.WriteLine("Четный массив до заданного числа");
+while(i<=a)
+
+if (i%2==0)
 {
-    max=c;
+    Console.WriteLine(i);
+    i+=1;
 }
-Console.WriteLine($"Максимальное число= {max}"); 
+else 
+{
+    i+=1;
+}
