f819db64815809afb35e6b7d200b737788c4677c
St <St@ya.ru>
2022-12-16 19:14:44 +0300

    первая задача

---
+++ dz1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n=Convert.ToInt32(Console.ReadLine());
+
+if (m>n) {
+    Console.WriteLine("Первое число больше второго");
+}
+else {
+    Console.WriteLine("Второе число больше первого");
+}
