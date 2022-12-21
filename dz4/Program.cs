37bb74dabb5d2b0d1073ccee8b14cd5a07b1559f
St <St@ya.ru>
2022-12-22 00:41:12 +0300

    добавлено домашнее задание 4

---
+++ dz4/Program.cs
@@ -0,0 +1,51 @@
+﻿// ЗАДАЧА 1
+
+// Console.WriteLine("Ввeдитe число А: ");
+// int a=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ввeдитe число B: ");
+// int b=Convert.ToInt32(Console.ReadLine());
+
+// double FunPow()
+// {
+//     double p=Math.Pow(a,b);
+//     return p;
+// }
+
+// Console.WriteLine($"Результат возведения в степень {FunPow()}");
+
+
+// ЗАДАЧА 2
+
+
+// Console.WriteLine("введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// int Func()
+// {
+// while (num> 0)
+// {
+// int temp = num % 10;
+// sum = sum + temp;
+// num=num/10;
+// }
+// return sum;
+// }
+// Console.WriteLine("сумма всех цифр числа равна: " + Func());
+
+
+// ЗАДАЧА 3
+
+// int [] print()
+// {
+// int [] array=new int [8];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write("Введите элемент массива \n");
+//         array[i]=Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// Console.WriteLine(string.Join(",", print()));
+
